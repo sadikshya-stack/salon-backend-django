408d96696ff6e87cf11fc4c5cf8e36d8b68efb61
sadikshya-stack <sadikshyasharma032@gmail.com>
2026-01-12 22:02:38 +0545

    alter table servicetype

--- booking/models.py
+++ booking/models.py
@@ -56,7 +56,7 @@ class ServiceType(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    duration_minutes = models.IntegerField()
+    duration_minutes = models.IntegerField(blank=True)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
